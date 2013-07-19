f9addd6567eb7d0164a81556007028ed88a04354
whu1p <whu@q.com>
2013-07-19 01:16:29 +0000

    Replaced deprecated call express.createserver() with call express()

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 var fs = require('fs');
 var express = require('express');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
+var app = express(express.logger());
 
 var outbuf = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
   response.send(outbuf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
